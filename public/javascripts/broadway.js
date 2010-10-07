2ae680e966c4dc2c4faed4bb29b57e2565538df3
Alexis Sukrieh <sukria@sukria.net>
2010-10-07 16:27:51 +0200

    display slide number in the footer

--- public/javascripts/broadway.js
+++ public/javascripts/broadway.js
@@ -26,9 +26,47 @@ function broadway_refresh() {
         url: "/refresh_slide",
         cache: false,
         success: function(html) {
+
+            // replace the slide content
             $('#slide').html(html);
+
+            // syntax highlighting
             prettyPrint();
+
+            // update the slide number (it's in the layout)
+            $.ajax({
+                type: 'POST', 
+                url: '/current_slide',
+                cache: false,
+                success: function(json) {
+                    $('#slide_number').html(json['slide'] + 1);
+            }});
         }
     });
 }
+
+/* init */
+var KEY_RIGHT = 39;
+var KEY_LEFT  = 37;
+var KEY_SPACE = 0;
+var KEY_ORIG  = 36;
+var KEY_END   = 35;
+
+$(document).ready(function() {
+
+    // register key bindings
+    $(document).keypress(function(e) {
+        if(e.keyCode == KEY_LEFT) {
+            broadway_prev();
+            broadway_refresh();
+        }
+        else if (e.keyCode == KEY_RIGHT) {
+            broadway_next();
+            broadway_refresh();
+        }
+    });
+
+    // display the current slide
+    broadway_refresh();
+});
 
