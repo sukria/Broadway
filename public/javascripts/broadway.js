25e5f5446a71778246162f22e1dfae50b4ff2a6e
mrpants <mr.pants@gmail.com>
2010-10-22 15:49:19 -0400

    Swapped keypress for keyup in JS; now works in Chrome and Safari.

--- public/javascripts/broadway.js
+++ public/javascripts/broadway.js
@@ -87,7 +87,7 @@ $(document).ready(function() {
 	window.pending_keypress = 0;
 
     // register key bindings
-    $(document).keypress(function(e) {
+    $(document).keyup(function(e) {
         if(e.keyCode == KEY_LEFT && (!window.pending_keypress)) {
 			window.pending_keypress = 1;
             broadway_prev();
